fb76c929f9909e9a03499dc2d1bf8cb7c77042be
Hau Le <lethehau90.vn@gmail.com>
2017-09-04 22:11:36 +0700

    update

--- src/providers/services/data.service.ts
+++ src/providers/services/data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { SystemConstants } from './../common/system.constants';
 import { AuthenService } from './authen.service';
-import { NotificationService } from './notification.service';
 import { UtilityService } from './utility.service';
 
 import { Observable } from 'rxjs/Observable';
@@ -12,7 +11,7 @@ import { MessageContstants } from './../common/message.constants';
 export class DataService {
   private headers: Headers;
   constructor(private _http: Http, private _authenService: AuthenService,
-    private _notificationService: NotificationService, private _utilityService: UtilityService) {
+    private _utilityService: UtilityService) {
     this.headers = new Headers();
     this.headers.append('Content-Type', 'application/json');
   }
@@ -70,17 +69,17 @@ export class DataService {
    public handleError(error: any) {
     if (error.status == 401) {
       localStorage.removeItem(SystemConstants.CURRENT_USER);
-      this._notificationService.printErrorMessage(MessageContstants.LOGIN_AGAIN_MSG);
+      alert(MessageContstants.LOGIN_AGAIN_MSG);
       this._utilityService.navigateToLogin();
     }
     else if (error.status == 403) {
       localStorage.removeItem(SystemConstants.CURRENT_USER);
-      this._notificationService.printErrorMessage(MessageContstants.FORBIDDEN);
+      alert(MessageContstants.FORBIDDEN);
       this._utilityService.navigateToLogin();
     }
     else {
       let errMsg = JSON.parse(error._body).Message;
-      this._notificationService.printErrorMessage(errMsg);
+      alert(errMsg);
 
       return Observable.throw(errMsg);
     }
