7ef50af61fa7efcc3e8f0e6c50aa9572731b7985
lethehau90.vn@gmail.com <lethehau90.vn@gmail.com>
2017-09-15 23:08:59 -0700

    update

--- src/providers/guards/auth.guard.ts
+++ src/providers/guards/auth.guard.ts
@@ -3,33 +3,37 @@ import { SystemConstants } from '../common/system.constants';
 //import { UrlConstants } from '../common/url.constants';
 
 @Injectable()
-export class AuthGuard  {
+export class AuthGuard {
     private isLoggedIn = false;
     constructor() {
 
     }
-    login(){
+    login() {
         if (localStorage.getItem(SystemConstants.CURRENT_USER)) {
-             this.isLoggedIn = true;
+            this.isLoggedIn = true;
         }
         else {
-             this.isLoggedIn = false;
+            this.isLoggedIn = false;
         }
-       
+
     }
 
-    logout(){
+    logout() {
         localStorage.removeItem(SystemConstants.CURRENT_USER)
         this.isLoggedIn = false;
-    } 
+    }
 
-    authGuard() : boolean{
+    get _isLoggedIn(): boolean {
+        return this.authGuard() == true ? true : false;
+    }
+
+    authGuard(): boolean {
         if (localStorage.getItem(SystemConstants.CURRENT_USER)) {
             return this.isLoggedIn = true;
         }
         else {
-           return  this.isLoggedIn = false;
+            return this.isLoggedIn = false;
         }
     }
-   
+
 }
